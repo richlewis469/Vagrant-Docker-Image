720cda3f2fca711ec1ef1c000edae257cf83d05b
Rich Lewis <rich.lewis@oracle.com>
2018-02-28 18:27:00 -0600

    Adding OVS

--- vagrant-shell/docker.sh
+++ vagrant-shell/docker.sh
@@ -3,17 +3,26 @@
 echo "Entering Docker Provisoning"
 date +"%F %T"
 
+# installing docker-engine vs docker
 yum install docker-engine \
   --assumeyes
 
 systemctl start docker
 systemctl enable docker
 
+# Enable IPv6 Support per https://docs.docker.com/config/daemon/ipv6/
 cat >> /etc/docker/daemon.json << EOF
 {
   "ipv6": true
 }
 EOF
+
+# https://stackoverflow.com/questions/23111631/cannot-download-docker-images-behind-a-proxy
+#mkdir /etc/systemd/system/docker.service.d
+#cat >> /etc/systemd/system/docker.service.d/http-proxy.conf << EOF
+#[Service]
+#Environment="HTTP_PROXY=http://adc-proxy.oracle.com:80/"
+#EOF
 
 systemctl reload docker
 
@@ -25,7 +34,7 @@ ip addr show
 usermod -aG docker vagrant
 
 docker run --name hello-world --rm hello-world
-docker image rm hello-world
+docker rmi hello-world
 
 date +"%F %T"
 echo "Exiting Docker Provisoning"
